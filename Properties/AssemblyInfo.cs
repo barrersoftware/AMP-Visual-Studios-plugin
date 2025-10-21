2904cd4511e113e4f85bec2db49a9d62ad495621
Daniel Elliott <ssfdre38@msn.com>
2025-10-21 16:46:50 -0700

    Update to v2.0.5 with correct version number
    - Fixed version in source.extension.vsixmanifest (2.0.5)
    - Updated AssemblyInfo to 2.0.5
    - Clean rebuild with VSSDK
    - Template now properly shows in VS New Project dialog
    - Working installation verified

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.4.0")]
-[assembly: AssemblyFileVersion("2.0.4.0")]
+[assembly: AssemblyVersion("2.0.5.0")]
+[assembly: AssemblyFileVersion("2.0.5.0")]
