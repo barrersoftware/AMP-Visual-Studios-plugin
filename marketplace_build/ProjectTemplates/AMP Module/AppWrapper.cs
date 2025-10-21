58c77cbc8c0b7c04415d1b664eca1c64c04b2018
Daniel Elliott <ssfdre38@msn.com>
2025-10-21 16:59:30 -0700

    Final marketplace-compatible VSIX v2.0.5
    - Uses exact CubeCoders marketplace structure
    - Includes .NET 8 Plugin template (new)
    - Includes AMP Module template (original CubeCoders)
    - Proper template manifest with both templates
    - Matches working marketplace VSIX format
    - Verified local installation working
    - Ready for marketplace upload

---
+++ marketplace_build/ProjectTemplates/AMP Module/AppWrapper.cs
@@ -0,0 +1,74 @@
+﻿using ModuleShared;
+using System;
+using System.Collections.Generic;
+
+//Remove the line below after creating your project!
+#pragma warning disable CS0067
+
+namespace AMPModuleTemplate
+{
+    public class AppWrapper : AppServerBase, IApplicationWrapper, IHasReadableConsole, IHasSimpleUserList, IHasWriteableConsole
+    {
+        public int MaxRAMUsage => 0;
+
+        public bool SupportsSleep => false;
+
+        public string ApplicationName => "Not Specified";
+
+        public string ModuleName => "Not Specified";
+
+        public string ModuleAuthor => "Not Specified";
+
+        public SupportedOS SupportedOperatingSystems => SupportedOS.Windows | SupportedOS.Linux;
+
+        public bool CanRunVirtualized => true;
+
+        public bool CanUpdateApplication => true;
+
+        public string BaseDirectory { get; set; }
+
+        public void Kill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Restart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Sleep()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<UserEventArgs> UserJoins;
+
+        public event EventHandler<UserEventArgs> UserLeaves;
+
+        public List<SimpleUser> Users { get; } = new List<SimpleUser>();
+
+        public int MaxUsers => 0;
+
+    }
+}
---
+++ marketplace_build/ProjectTemplates/AMP Module/ModuleMain.cs
@@ -0,0 +1,44 @@
+﻿using ModuleShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AMPModuleTemplate
+{
+    public class ModuleMain : AppModule
+    {
+        private readonly ILogger log;
+        private readonly IConfigSerializer config;
+        private readonly SupportedOS currentPlatform;
+        private readonly IRunningTasksManager taskManager;
+
+        public override bool HasFrontendContent => false;
+
+        public override IEnumerable<SettingStore> SettingStores => Enumerable.Empty<SettingStore>();
+
+        public IPluginMessagePusher Message { get; }
+        public IFeatureManager Features { get; }
+
+        public ModuleMain(ILogger log, IConfigSerializer config, SupportedOS currentPlatform, 
+            IRunningTasksManager taskManager, IPluginMessagePusher Message, IFeatureManager Features)
+        {
+            this.log = log;
+            this.config = config;
+            this.currentPlatform = currentPlatform;
+            this.taskManager = taskManager;
+            this.Message = Message;
+            this.Features = Features;
+        }
+
+        public override void Init(out IApplicationWrapper Application, out WebMethodsBase APIMethods) //Mandatory Args
+        {
+            Application = new AppWrapper();
+            APIMethods = null;
+        }
+
+        public override void PostInit()
+        {
+            
+        }
+    }
+}
