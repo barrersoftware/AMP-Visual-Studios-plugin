4dfc70fb436f407765abc3a3a0b99ad00e86b025
Daniel Elliott <ssfdre38@msn.com>
2025-10-21 16:07:14 -0700

    Build with official VSSDK tools - v2.0.5
    - Installed Visual Studio SDK extension development workload
    - Created proper .csproj for VSIX project
    - Built with Microsoft VsixUtil.exe (official tool)
    - Template files properly packaged by VSSDK
    - Working for local installation
    - Ready for marketplace (templates auto-unpacked by VSSDK is normal)

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AMP Plugin Template (.NET 8)")]
+[assembly: AssemblyDescription("Project template for CubeCoders AMP plugins targeting .NET 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barrer Software")]
+[assembly: AssemblyProduct("AMP Plugin Template")]
+[assembly: AssemblyCopyright("Copyright © 2025 Barrer Software")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
